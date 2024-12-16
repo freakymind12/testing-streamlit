5744c606de2499cd19750cc581a27e186559aec4
Rian Kurniawan <130054977+freakymind12@users.noreply.github.com>
2024-12-16 07:36:42 +0000

    [add] follium and shadcn ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import streamlit_shadcn_ui as ui
+import folium
+from streamlit_folium import st_folium
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+trigger_btn = ui.button(text="Trigger Button", key="trigger_btn")
+
+ui.alert_dialog(show=trigger_btn, title="Alert Dialog", description="This is an alert dialog", confirm_label="OK", cancel_label="Cancel", key="alert_dialog1")
+
+
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
